d6bceef7a6e87b008444799b2295d65413fb7aec
upamid <dstupny@gmail.com>
2021-06-25 17:30:16 +0700

    second_deploy

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось! Ура!')
+    return HttpResponse('У меня получилось! Ура! A')
 
 
 def second_page(request):

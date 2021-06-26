1d30de0f74addffdaea638335c17d36542a439de
upamid <dstupny@gmail.com>
2021-06-26 15:47:27 +0700

    test

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось! Ура! A')
+    return HttpResponse('У меня получилось!')
 
 
 def second_page(request):
